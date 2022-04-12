67711e646213b90301fa66c8a722e15fa80c4ae1
Spencer Robinson <spencergrobinson@gmail.com>
2022-04-12 09:56:23 -0400

    various improvements to particles

--- js/particles.js
+++ js/particles.js
@@ -13,12 +13,12 @@ class Particle {
 }
 
 const canvas = document.getElementById("canvas");
-const num_particles = 30
+const num_particles = 50
 const fps = 30
 
 var dead_particles = []
 var particles = []
-var particle_color = "#FFFFFF"
+var particle_color = "#FFFFFFAA"
 var last_time = new Date();
 
 for (var i = 0; i < num_particles; i++)
@@ -31,8 +31,14 @@ tick_particle_func = dummy_func;
 draw_particle_func = dummy_func;
 
 function particle_drawer() {
-  canvas.width = canvas.parentElement.clientWidth
-  canvas.height = canvas.parentElement.clientHeight
+  window.setTimeout(function() {window.requestAnimationFrame(particle_drawer)}, 1000/fps);
+
+  var height = Math.max(canvas.parentElement.clientHeight, document.children[0].scrollHeight)
+  var width = Math.max(canvas.parentElement.clientWidth, document.children[0].scrollWidth)
+  if (width != canvas.width)
+    canvas.width = width;
+  if (height != canvas.height)
+    canvas.height = height;
 
   if (dead_particles.length > 0) {
     var last_particle = dead_particles.length - 1;
@@ -48,9 +54,10 @@ function particle_drawer() {
   var new_time = new Date()
   var dt = new_time - last_time;
   last_time = new_time;
-
   for (var i = 0; i < particles.length; i++) {
     draw_particle_func(particles[i], ctx, true);
+  }
+  for (var i = 0; i < particles.length; i++) {
     remove = tick_particle_func(particles[i], dt);
 
     if (remove) {
@@ -60,29 +67,39 @@ function particle_drawer() {
       draw_particle_func(particles[i], ctx, false);
     }
   }
-
-  window.setTimeout(function() {window.requestAnimationFrame(particle_drawer)}, 1000/fps);
 }
 window.requestAnimationFrame(particle_drawer);
 
 
 
-function top_spawn(p) {
+function top_and_left_spawn(p) {
   if (Math.random() < 0.90) 
     return false;
-  p.x = Math.floor(Math.random() * canvas.width);
-  p.y = 0;
+
   p.dx = 0.2 + Math.random() * 0.4;
   p.dy = 1 + Math.random() * 1;
-  p.radius = Math.floor(2 * Math.random()) + 2
+  p.radius = Math.floor(2 * Math.random()) + 3
   p.opacity = 0.5 + Math.random() / 2
+
+  var height_weight = p.dx * canvas.height;
+  var width_weight = p.dy * canvas.width;
+
+  var start = Math.random() * (height_weight + width_weight);
+  if (start < height_weight) {
+    p.x = 0;
+    p.y = (start/height_weight) * canvas.height;
+  } else {
+    p.x = ((start - height_weight)/width_weight) * canvas.width;
+    p.y = 0;
+  }
   return true;
 }
 
 function draw_circle(p, ctx, undraw) {
   if (undraw) {
-    var extra = 2;
-    ctx.clearRect(p.x - p.radius - extra, p.y - p.radius - extra, (p.radius + extra) * 2, (p.radius + extra) * 2);
+    var extra = 1;
+    var edge = p.radius + extra
+    ctx.clearRect(p.x - edge, p.y - edge, edge * 2, edge * 2);
   } else {
     ctx.beginPath();
     ctx.arc(p.x, p.y, p.radius, 0, Math.PI * 2, true);
@@ -98,6 +115,6 @@ function constant_speed(p, dt) {
   return p.y > canvas.height || p.x < 0 || p.x > canvas.width;
 }
 
-spawn_particle_func = top_spawn
+spawn_particle_func = top_and_left_spawn
 tick_particle_func = constant_speed;
 draw_particle_func = draw_circle;
